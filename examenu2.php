8f5b55de5a79358bc1d67090614374ae3cb86da9
CarlosRuiz02 <60682882+CarlosRuiz02@users.noreply.github.com>
2020-02-27 15:02:18 -0600

    Add files via upload

---
+++ examenu2.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MULTIPLICAR</title>
+</head>
+
+<body>
+	<h1>MULTIPLICADOR</h1>
+<select name="tabla">
+    <option>Tablas de multiplicar</option> 
+    <option value="1">1</option> 
+    <option value="2">2</option> 
+    <option value="3">3</option> 
+    <option value="4">4</option> 
+    <option value="5">5</option> 
+    <option value="6">6</option> 
+    <option value="7">7</option> 
+    <option value="8">8</option> 
+    <option value="9">9</option> 
+    <option value="10">10</option> 
+    <option value="11">11</option> 
+    <option value="12">12</option> 
+</select>
+<input type="number" name="multiplicador" placeholder="multiplicador">
+<input type="submit" value="Generador">
+<?php 
+
+
+
+
+
+echo "<br>";
+echo "<br>";
+?>
+
+<div class="table-responsive">
+<table class="table table-bordered table-striped">
+<thead class="thead-dark">
+    <th width="18%">tabla</th>
+    <th width="22%">multiplicador</th>
+    <th width="22%">resultado</th>
+</thead>
+
+
+</body>
+</html>
