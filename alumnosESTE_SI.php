41bfbc503b9c6edd3a7daba4d943dfa568a1c286
CarlosRuiz02 <60682882+CarlosRuiz02@users.noreply.github.com>
2020-03-03 14:50:59 -0600

    Add files via upload

---
+++ alumnosESTE_SI.php
@@ -0,0 +1,54 @@
+<h1>INGRESA LOS DATOS</h1>
+<form action="alumnos.php" method="POST">
+	Alumno:
+	<input type="text" name="alumno[nombre]" required>
+	<br>
+	Matricula:
+	<input type="text" name= "alumno[matricula]" required>
+	<br>
+	Edad:
+	<input type="number" name="alumno[matricula]" required>
+	<br>
+	Carrera:
+	<input type="text" name ="alumno[carrera]" required>
+	<br>
+	Email:
+	<input type="text" name ="alumno[email]" required>
+	<br>
+	Telefono:
+	<input type="text" name ="alumno[telefono]" required>
+	<br>
+	Tutor:
+	<input type="text" name ="alumno[tutor]" required>
+	<input type="submit" value="enviar">
+</form>
+<?php 
+if (isset($_POST['alumno'])){
+	$array=$_POST['alumno'];
+	$nombre=$array['nombre'];
+	$matricula=$array['matricula'];
+	$carrera=$array['carrera'];
+	$email=$array['email'];
+	$telefono=$array['telefono'];
+	$tutor=$array['tutor'];
+}
+
+if ($matricula>100) {
+	if ($matricula<1000) {
+		echo "Los datos del alumno son:<br>";
+		echo "Nombre: $nombre <br>";
+		echo "Matricula: $matricula <br>";
+		echo "Carrera: $carrera <br>";
+		echo "Email: $email <br>";
+		echo "Telefono: $telefono <br>";
+		echo "Tutor: $tutor <br>";
+	}else{
+		echo "La matricula esta fuera de rango. Matricula:$matricula";
+	}
+
+}else{
+	echo "La matricula esta fuera de rango. Matricula:$matricula";
+}
+
+
+?>
